e3d4801d6634426d2af53228a6f5a79463376a16
jvmoraveis <jvmoraveis@squaretrade.com>
2019-08-19 07:30:07 -0700

    SVC non-linear and decision tree

--- chapter3/main.py
+++ chapter3/main.py
@@ -46,29 +46,32 @@ from matplotlib.colors import ListedColormap
 import matplotlib.pyplot as plt
 
 def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
+    
     # setup marker generator and color map
     markers = ('s', 'x', 'o', '^', 'v')
     colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
     cmap = ListedColormap(colors[:len(np.unique(y))])
-
+    
     # plot the decision surface
     x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-    x2_min, x2_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution))
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),np.arange(x2_min, x2_max, resolution))
     Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
     Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
     plt.xlim(xx1.min(), xx1.max())
     plt.ylim(xx2.min(), xx2.max())
 
     # plot all samples
     X_test, y_test = X[test_idx, :], y[test_idx]
     for idx, cl in enumerate(np.unique(y)):
-        plt.scatter(x=X[y==cl, 0], y=X[y==cl, 1], alpha=0.8, c=cmap(idx), marker=markers[idx], label=cl)
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1], alpha=0.8, c=cmap(idx), marker=markers[idx], label=cl)
     
     # highlight test samples
     if test_idx:
         X_test, y_test = X[test_idx, :], y[test_idx]
-        plt.scatter(X_test[:, 0], X_test[:, 1], c='', alpha=1.0, linewidth=1, marker='o', s=55, label='test set') 
+        plt.scatter(X_test[:, 0], X_test[:, 1], c='', alpha=1.0, linewidth=1, marker='o', s=55, label='test set')
+
 
 
 #%%
@@ -148,4 +151,99 @@ lr = SGDClassifier(loss='log')
 svm = SGDClassifier(loss='hinge')
 
 
-#%% 
+#%% Solving nonlinear problems
+np.random.seed(0)
+X_xor = np.random.randn(200, 2)
+y_xor = np.logical_xor(X_xor[:, 0] > 0, X_xor[:, 1] > 0)
+y_xor = np.where(y_xor, 1, -1)
+plt.scatter(X_xor[y_xor==1, 0], X_xor[y_xor==1, 1], c='b', marker='x', label='1')
+plt.scatter(X_xor[y_xor==-1, 0], X_xor[y_xor==-1, 1], c='r', marker='s', label='-1')
+plt.ylim(-3.0)
+plt.legend()
+plt.show()
+
+#%% SVM with Radial Basic Function (RBF) kernel for non linear problems
+svm = SVC(kernel='rbf', random_state=0, gamma=0.10, C=10.0)
+svm.fit(X_xor, y_xor)
+plot_decision_regions(X_xor, y_xor, classifier=svm)
+plt.legend(loc='upper left')
+plt.show()
+
+
+#%% applied SVM model for Iris dataset with RBF kernel and small gamma
+svm = SVC(kernel='rbf', random_state=0, gamma=0.2, C=1.0)
+svm.fit(X_train_std, y_train)
+plot_decision_regions(X_combined_std, y_combined_std, classifier=svm, test_idx=range(105, 150))
+plt.xlabel('petal lenght [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.show()
+
+
+#%% applied SVM model for Iris dataset with RBF kernel and higher gamma
+svm = SVC(kernel='rbf', random_state=0, gamma=100.0, C=1.0)
+svm.fit(X_train_std, y_train)
+plot_decision_regions(X_combined_std, y_combined_std, classifier=svm, test_idx=range(105, 150))
+plt.xlabel('petal lenght [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.show()
+
+
+
+#%% Decision tree learning - Gini index
+def gini(p):
+    return (p)*(1 - (p)) + (1 - p) * (1 - (1-p))
+
+def entropy(p):
+    return -p * np.log2(p) - (1 - p) * np.log2((1-p))
+
+def error(p):
+    return 1 - np.max([p, 1 - p])
+
+x = np.arange(0.0, 1.0, 0.01)
+
+ent = [entropy(p) if p != 0 else None for p in x]
+sc_ent = [e*0.5 if e else None for e in ent]
+err = [error(i) for i in x]
+
+fig = plt.figure()
+ax = plt.subplot(111)
+
+for i, lab, ls, c, in zip([ent, sc_ent, gini(x), err],
+    ['Entropy', 'Entropy (scaled)','Gini Impurity', 'Missclassification Error'],
+    ['-', '-', '--', '-.'],
+    ['blue', 'lightgray', 'red', 'green', 'cyan']):
+    
+    line = ax.plot(x, i, label=lab, linestyle=ls, lw=2, color=c)
+
+ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.15), ncol=3, fancybox=True, shadow=False)
+ax.axhline(y=0.5, linewidth=1, color='k', linestyle='--')
+ax.axhline(y=1.0, linewidth=1, color='k', linestyle='--')
+plt.ylim([0, 1.1])
+plt.xlabel('p(i=1)')
+plt.ylabel('Impurity Index')
+plt.show()
+
+
+#%% building a decesion tree
+from sklearn.tree import DecisionTreeClassifier
+
+tree = DecisionTreeClassifier(criterion='entropy', max_depth=3, random_state=0)
+tree.fit(X_train, y_train)
+X_combined = np.vstack((X_train, X_test))
+y_combined = np.hstack((y_train, y_test))
+
+
+#%% ploting decision tree
+plot_decision_regions(X_combined, y_combined, classifier=tree, test_idx=range(105, 150))
+plt.xlabel('petal lenght [cm]')
+plt.ylabel('pertal width [cm]')
+plt.legend(loc='upper left')
+plt.show()
+
+#%%
+from sklearn.tree import export_graphviz
+export_graphviz(tree, out_file='tree.dot', feature_names=['petal lenght', 'petal width'])
+
+#%%
