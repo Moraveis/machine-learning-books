f1a6d4677bc971c2f6b7d20fde15ecb93fc9d298
Moraveis <joaovitormoraveis@gmail.com>
2019-08-24 20:44:26 -0300

    Finishing chapter 3

--- chapter3/main.py
+++ chapter3/main.py
@@ -246,4 +246,17 @@ plt.show()
 from sklearn.tree import export_graphviz
 export_graphviz(tree, out_file='tree.dot', feature_names=['petal lenght', 'petal width'])
 
+#%% Working with RandomForest
+from sklearn.ensemble import RandomForestClassifier
+forest = RandomForestClassifier(criterion='entropy', n_estimators=10, random_state=1, n_jobs=2)
+forest.fit(X_train, y_train)
+
+
+#%%
+plot_decision_regions(X_combined, y_combined, classifier=forest, test_idx=range(105, 150))
+plt.xlabel('petal length')
+plt.ylabel('petal width')
+plt.legend(loc='upper left')
+plt.show()
+
 #%%
