d4740825bd78be62f059a6ba48987632a994dec2
Moraveis <joaovitormoraveis@gmail.com>
2019-09-10 08:34:22 -0300

    PCA and LDA implementation

--- chapter5/main.py
+++ chapter5/main.py
@@ -58,18 +58,92 @@ print('Matrix W:\n', w)
 X_train_std[0].dot(w)
 
 #%%
-X_train_pca = X_train_std[0].dot(w)
+X_train_pca = X_train_std.dot(w)
 
 #%% visualize the transformed Wine training 
 colors = ['r', 'b', 'g']
 markers = ['s', 'x', 'o']
 for l, c, m in zip(np.unique(y_train), colors, markers):
-    plt.scatter(X_train_pca[y_train==l, 0], X_train_pca[y_train==l, 1], c=c, label=l, marker=m)
+    plt.scatter(x=X_train_pca[y_train==l, 0], y=X_train_pca[y_train==l, 1], c=c, label=l, marker=m)
 plt.xlabel('PC 1')
 plt.ylabel('PC 2')
 plt.legend(loc='lower left')
 plt.show()
 
 
+
+#%%
+from matplotlib.colors import ListedColormap
+
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+    
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+        np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # plot class samples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1], alpha=0.8, c=cmap(idx), marker=markers[idx], label=cl)
+    
+
+#%% PCA from sklearn
+from sklearn.linear_model import LogisticRegression
+from sklearn.decomposition import PCA
+
+pca = PCA(n_components=2)
+lr = LogisticRegression()
+X_train_pca = pca.fit_transform(X_train_std)
+X_test_pca = pca.transform(X_test_std)
+lr.fit(X_train_pca, y_train)
+
+plot_decision_regions(X_train_pca, y_train, classifier=lr)
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.legend(loc='lower left')
+plt.show()
+
+#%%
+plot_decision_regions(X_test_pca, y_test, classifier=lr)
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.legend(loc='lower left')
+plt.show()
+
+#%%
+pca = PCA(n_components=None)
+X_train_pca = pca.fit_transform(X_train_std)
+pca.explained_variance_ratio_
+
+#%%
+np.set_printoptions(precision=4)
+mean_vecs = []
+for label in range(1,4):
+    mean_vecs.append(np.mean(X_train_std[y_train==label], axis=0))
+    print('MV %s: %s\n' % (label, mean_vecs[label-1]))
+
+#%%
+d = 13 #number of features
+S_W = np.zeros((d,d))
+
+for label, mv in zip(range(1,4), mean_vecs):
+    class_scatter = np.zeros((d,d))
+    for row in X[y == label]:
+        row, mv = row.reshape(d, 1), mv.reshape(d, 1)
+        class_scatter += (row-mv).dot((row-mv).T)
+    S_W += class_scatter
+
+print('Within-class scatter matrix: %sx%s' % (S_W.shape[0], S_W.shape[1]))
 
 #%%
